3ce422d8df36d24125da56484fdb1e85f6bab36c
Yang Zhou <yzhou3991@gmail.com>
2021-01-19 12:06:16 -0500

    add floor when pressed a button

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import {getRandomIntInclusive} from "./helper/utilityFunctions"
 function App() {
   // initialize a random floow between 1 and 10
   const [currentFloor,setCurrentFloor] = useState(getRandomIntInclusive(1,10))
+  const [floorsToGo,setFloorsToGo] = useState([])
+  const floors = Array(10).fill(null).map((_, i) => i+1);
+  const selectFloor = (e)=>{
+    floorsToGo.push(e.target.value)
+    console.log(floorsToGo)
+    setFloorsToGo(floorsToGo)
+  }
   return (
     <div className="App">
-      <h2>this is floor {currentFloor}</h2>
+      <h2>current floor: {currentFloor}</h2>
+      <h2>floors to go: {floorsToGo}</h2>
+      <h2>Select a floor to go:</h2>
+      {floors.map((floor)=>(
+        <button onClick={selectFloor} value ={floor}>{floor}</button>
+      ))}
     </div>
   );
 }
