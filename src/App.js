297511608f5f0e43f0f6f6be3a67c23644c9a4db
Yang Zhou <yzhou3991@gmail.com>
2021-01-19 11:49:14 -0500

    initialize a random floor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect,useState} from 'react'
+import {getRandomIntInclusive} from "./helper/utilityFunctions"
 function App() {
+  // initialize a random floow between 1 and 10
+  const [currentFloor,setCurrentFloor] = useState(getRandomIntInclusive(1,10))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>this is floor {currentFloor}</h2>
     </div>
   );
 }
